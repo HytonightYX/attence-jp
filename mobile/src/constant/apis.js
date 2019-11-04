6902d17c45d25e251aa78b762563aad338e8f97c
liyangtom <quanta2006@163.com>
2019-11-04 20:05:54 +0800

    fix package

--- mobile/src/constant/apis.js
+++ mobile/src/constant/apis.js
@@ -1,12 +1,12 @@
 var mode = process.env.REACT_APP_ENV
-var API_SERVER = 'http://47.111.22.103:8080'
+var API_SERVER = 'http://133.167.73.231:8080'
 
 if (mode === 'development') {
   API_SERVER = 'http://127.0.0.1:8080'
 }
 
 if (mode === 'production') {
-  API_SERVER = 'http://47.111.22.103:8080'
+  API_SERVER = 'http://133.167.73.231:8080'
 }
 
 export { API_SERVER }
