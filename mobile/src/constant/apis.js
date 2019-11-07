46c717beb218bed66580595ead74cdcb1ec4f1e8
liyangtom <quanta2006@163.com>
2019-11-07 14:23:29 +0800

    修改定位模式

--- mobile/src/constant/apis.js
+++ mobile/src/constant/apis.js
@@ -1,12 +1,12 @@
 var mode = process.env.REACT_APP_MY_VAR
-var API_SERVER = 'http://133.167.73.231:8080'
+var API_SERVER = 'https://133.167.73.231:8080'
 
 if (mode === 'development') {
   API_SERVER = 'http://127.0.0.1:8080'
 }
 
 if (mode === 'production') {
-  API_SERVER = 'http://133.167.73.231:8080'
+  API_SERVER = 'https://133.167.73.231:8080'
 }
 
 export { API_SERVER }
--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+var https = require('https')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const fs = require('fs')
@@ -78,4 +79,32 @@ app.post('/Login', async function (req, res) {
 	})
 })
 
-app.listen(port, () => console.log(`> Running on localhost:${port}`))
+
+app.get('/loc', async function (req, res) {
+	console.log('loc')
+	let lat = req.query.lat
+  let lng = req.query.lng
+	let host = 'https://maps.googleapis.com/maps/api/geocode'
+	let key = 'AIzaSyBSWEVNt2h8CrDWufKNy32k-t14tnW9D9I'
+	let url = `${host}/json?language=ja&key=${key}&latlng=${lat},${lng}`
+	const r = await axios.get(url)
+	if (r && r.status === 200) {
+	  // console.log(r.data)
+	  console.log(r.data.results[0].formatted_address)
+	  res.status(200).json({code: 200, data: r.data.results[0].formatted_address, msg: '登录成功'})
+	}else{
+	  res.status(200).json({code: 200, data: '获取地址失败', msg: '登录成功'})
+	}
+	
+})
+
+
+
+
+// app.listen(port, () => console.log(`> Running on localhost:${port}`))
+
+var options = {
+  key:fs.readFileSync('/usr/local/key/1897739_manqc.site.key'),
+  cert:fs.readFileSync('/usr/local/key/1897739_manqc.site.pem')
+}
+https.createServer(options,app).listen(443)
