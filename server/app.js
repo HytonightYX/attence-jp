9b6a8de6c5bc36106c6847439ec23072c0822a49
HytonightYX <whosiyuan@qq.com>
2019-11-07 23:14:22 +0800

    hsy: 打卡后端接口

--- server/app.js
+++ server/app.js
@@ -1,5 +1,4 @@
 const express = require('express')
-var https = require('https')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const fs = require('fs')
@@ -79,30 +78,44 @@ app.post('/Login', async function (req, res) {
 	})
 })
 
+/**
+ * 打卡接口
+ */
+app.post('/Clock', async function (req, res) {
+	let params = req.body
 
-// app.get('/loc', async function (req, res) {
-// 	console.log('loc')
-// 	let lat = req.query.lat
-//   let lng = req.query.lng
-// 	let host = 'https://maps.googleapis.com/maps/api/geocode'
-// 	let key = 'AIzaSyBSWEVNt2h8CrDWufKNy32k-t14tnW9D9I'
-// 	let url = `${host}/json?language=ja&key=${key}&latlng=${lat},${lng}`
-// 	const r = await axios.get(url)
-// 	if (r && r.status === 200) {
-// 	  // console.log(r.data)
-// 	  console.log(r.data.results[0].formatted_address)
-// 	  res.status(200).json({code: 200, data: r.data.results[0].formatted_address, msg: '登录成功'})
-// 	}else{
-// 	  res.status(200).json({code: 200, data: '获取地址失败', msg: '登录成功'})
-// 	}
-	
-// })
+	let procs = [
+		'PROC_USER_CLOCK_INFO',
+		'PROC_USER_CLOCK_IN',
+		'PROC_USER_CLOCK_OUT'
+	]
 
+	let status = params.status
+	let sql = `CALL ${procs[status]}(?)`
+
+	params.clock_date = utils.newDateTime('YYYYMMDD000000')
+	params.clock_time = utils.newDateTime('YYYYMMDDhhmm00')
+
+	callProc(sql, params, res, (r) => {
+		console.log(r)
+		res.status(200).json({code: 301, data: r, msg: '打卡成功'})
+	})
+})
+
+/**
+ * 获取打卡信息
+ * 用户每次进入打卡页面都会调取
+ */
+app.post('/ClockInfo', async function (req, res) {
+	let sql = `CALL PROC_USER_CLOCK_INFO(?)`
+	let params = req.body
+	params.create_at = utils.newDateTime()
+	params.clock_date = utils.newDateTime('YYYYMMDD000000')
+
+	callProc(sql, params, res, (r) => {
+		console.log(r)
+		res.status(200).json({code: 200, data: r, msg: '获取成功'})
+	})
+})
 
 app.listen(port, () => console.log(`> Running on localhost:${port}`))
-
-// var options = {
-//   key:fs.readFileSync('/usr/local/key/1897739_manqc.site.key'),
-//   cert:fs.readFileSync('/usr/local/key/1897739_manqc.site.pem')
-// }
-// https.createServer(options,app).listen(443)
